f25d0e776964f55c34a08479c0a0d502af0c1be8
hamt3ch <hmiles23@ufl.edu>
2015-09-15 10:32:17 -0400

    Created checkEndGame() and clearBoard()

--- TicTacToe_HAM/TicTacToe_HAM/GameBoard.swift
+++ TicTacToe_HAM/TicTacToe_HAM/GameBoard.swift
@@ -30,7 +30,12 @@ class GameBoard: UIViewController {
     @IBOutlet var Pos7: UIImageView!
     @IBOutlet var Pos8: UIImageView!
     
+    let cross : UIImage = UIImage(named:"tic-tac-toe-X")!
+    let circle : UIImage = UIImage(named:"tic-tac-toe-O")!
+    
     var currentUser:String = "player1"
+    var checkUsers: [String] = ["player1", "player2"]
+    var myBoard : [[String]] = [["0","1","2"],["3","4","5"],["6","7","8"]]
 
     
     override func viewDidLoad() {
@@ -50,45 +55,114 @@ class GameBoard: UIViewController {
         let circle : UIImage = UIImage(named:"tic-tac-toe-O")!
         var playerPiece : UIImage
         
-        if (currentUser == "player1"){
+        if (self.currentUser == "player1"){
             playerPiece = cross // set piece = X (Player1)
-            self.currentUser = "player2"    //tgl user
         }
         else {
             playerPiece = circle //set piece = O (Player2)
-            self.currentUser = "player1"    //tgl user
         }
 
         switch(sender.tag){ // parse which button has been touched
         case 0:
             print("Button 0 Pressed")
             Pos0.image = playerPiece
+            myBoard[0][0] = self.currentUser
         case 1:
             print("Button 1 Pressed")
             Pos1.image = playerPiece
+            myBoard[0][1] = self.currentUser
         case 2:
             print("Button 2 Pressed")
             Pos2.image = playerPiece
+            myBoard[0][2] = self.currentUser
         case 3:
             print("Button 3 Pressed")
             Pos3.image = playerPiece
+            myBoard[1][0] = self.currentUser
         case 4:
             print("Button 4 Pressed")
             Pos4.image = playerPiece
+            myBoard[1][1] = self.currentUser
         case 5:
             print("Button 5 Pressed")
             Pos5.image = playerPiece
+            myBoard[1][2] = self.currentUser
         case 6:
             print("Button 6 Pressed")
             Pos6.image = playerPiece
+            myBoard[2][0] = self.currentUser
         case 7:
             print("Button 7 Pressed")
             Pos7.image = playerPiece
+            myBoard[2][1] = self.currentUser
         case 8:
             print("Button 8 Pressed")
             Pos8.image = playerPiece
+            myBoard[2][2] = self.currentUser
         default:
             print("Error in userPlacedPiece")
         }
+        
+        if (self.currentUser == "player1"){
+            self.currentUser = "player2"    //tgl user user
+        }
+        else {
+            self.currentUser = "player1"    //tgl user
+        }
+        
+        checkForWinner() // check if game is complete
+        
+    }
+    
+    func checkForWinner () {
+        
+        for users in checkUsers {
+
+            //Horizontally Row1
+            if(myBoard[0][0] == users && myBoard[0][1] == users && myBoard[0][2] == users) {
+                print("Top Row is filled")
+            }
+            
+            //Horizontally Row2
+            if(myBoard[1][0] == users && myBoard[1][1] == users && myBoard[1][2] == users) {
+                print("Middle Row is filled")
+            }
+            
+            //Horizontally Row3
+            if(myBoard[2][0] == users && myBoard[2][1] == users && myBoard[2][2] == users) {
+                print("Bottom Row is filled")
+            }
+            
+            
+            //Vertically Col1
+            if(myBoard[0][0] == users && myBoard[1][0] == users && myBoard[2][0] == users) {
+                print("Left Column is filled")
+            }
+            
+            //Vertically Col2
+            if(myBoard[1][0] == users && myBoard[1][1] == users && myBoard[1][2] == users) {
+                print("Left Column is filled")
+            }
+            
+            //Vertically Col3
+            if(myBoard[2][0] == users && myBoard[2][1] == users && myBoard[2][2] == users) {
+                print("Left Column is filled")
+            }
+            
+            //Diagonally Right >> Left
+            if(myBoard[0][0] == users && myBoard[1][1] == users && myBoard[2][2] == users) {
+                print("Right Diagonal")
+            }
+            
+            //Diagonally Left >> Right
+            if(myBoard[0][2] == users && myBoard[1][1] == users && myBoard[2][0] == users) {
+                print("Right Diagonal")
+            }
+        }
+    }
+    
+    func clearBoard() {
+       myBoard = [["0","1","2"],["3","4","5"],["6","7","8"]]
+       Pos0.image = nil
     }
 }
