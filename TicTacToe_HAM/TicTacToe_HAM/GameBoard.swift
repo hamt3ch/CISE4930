c35a5a8c5d51e77e0ea0866a1e6ab805c509276d
hamt3ch <hmiles23@ufl.edu>
2015-09-14 21:18:03 -0400

    Added Dynamic touch placement for UI

--- TicTacToe_HAM/TicTacToe_HAM/GameBoard.swift
+++ TicTacToe_HAM/TicTacToe_HAM/GameBoard.swift
@@ -16,26 +16,26 @@ class GameBoard: UIViewController {
     //Positions going down >> left to right
     
     //FirstRow
-    @IBOutlet var pos0: UIImageView!
-    @IBOutlet var pos1: UIImageView!
-    @IBOutlet var pos2: UIImageView!
+    @IBOutlet var Pos0: UIImageView!
+    @IBOutlet var Pos1: UIImageView!
+    @IBOutlet var Pos2: UIImageView!
     
     //SecondRow
-    @IBOutlet var pos3: UIImageView!
-    @IBOutlet var pos4: UIImageView!
-    @IBOutlet var pos5: UIImageView!
+    @IBOutlet var Pos3: UIImageView!
+    @IBOutlet var Pos4: UIImageView!
+    @IBOutlet var Pos5: UIImageView!
 
     //ThirdRow
-    @IBOutlet var pos6: UIImageView!
-    @IBOutlet var pos7: UIImageView!
-    @IBOutlet var pos8: UIImageView!
-    
+    @IBOutlet var Pos6: UIImageView!
+    @IBOutlet var Pos7: UIImageView!
+    @IBOutlet var Pos8: UIImageView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var circle : UIImage = UIImage(named:"tic-tac-toe-O")!
-        pos0.image = circle
+        //pos0.image = circle
     }
     
     override func didReceiveMemoryWarning() {
@@ -43,37 +43,45 @@ class GameBoard: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func setPiece(position:UIImageView, shape:String){ //places pieces in certain positions
-        
-    }
-    
     @IBAction func userPlacedPiece(sender: AnyObject) {
-        var value:NSString = sender.currentTitle!! // unwrap button
-        var intVal:Int = value.integerValue
-        print(intVal)
-        switch(intVal){
+        let cross : UIImage = UIImage(named:"tic-tac-toe-X")!
+        let circle : UIImage = UIImage(named:"tic-tac-toe-O")!
+        
+        //pos0.image = circle
+        
+        switch(sender.tag){ // parse which button has been touched
         case 0:
             print("Button 0 Pressed")
+            Pos0.image = circle
         case 1:
             print("Button 1 Pressed")
+            Pos1.image = circle
         case 2:
             print("Button 2 Pressed")
+            Pos2.image = circle
         case 3:
             print("Button 3 Pressed")
+            Pos3.image = circle
         case 4:
             print("Button 4 Pressed")
+            Pos4.image = circle
         case 5:
             print("Button 5 Pressed")
+            Pos5.image = circle
         case 6:
             print("Button 6 Pressed")
+            Pos6.image = circle
         case 7:
             print("Button 7 Pressed")
+            Pos7.image = circle
         case 8:
             print("Button 8 Pressed")
+            Pos8.image = circle
         default:
             print("Error in userPlacedPiece")
 
         }
+        
     }
   
     
