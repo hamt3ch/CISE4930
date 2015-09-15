3ed2f56e6afbf4d087c7c8c3f1ff2d9994d63821
hamt3ch <hmiles23@ufl.edu>
2015-09-14 21:58:26 -0400

    Added Player toggle piece changes

--- TicTacToe_HAM/TicTacToe_HAM/GameBoard.swift
+++ TicTacToe_HAM/TicTacToe_HAM/GameBoard.swift
@@ -29,6 +29,8 @@ class GameBoard: UIViewController {
     @IBOutlet var Pos6: UIImageView!
     @IBOutlet var Pos7: UIImageView!
     @IBOutlet var Pos8: UIImageView!
+    
+    var currentUser:String = "player1"
 
     
     override func viewDidLoad() {
@@ -46,43 +48,47 @@ class GameBoard: UIViewController {
     @IBAction func userPlacedPiece(sender: AnyObject) {
         let cross : UIImage = UIImage(named:"tic-tac-toe-X")!
         let circle : UIImage = UIImage(named:"tic-tac-toe-O")!
+        var playerPiece : UIImage
         
-        //pos0.image = circle
-        
+        if (currentUser == "player1"){
+            playerPiece = cross // set piece = X (Player1)
+            self.currentUser = "player2"    //tgl user
+        }
+        else {
+            playerPiece = circle //set piece = O (Player2)
+            self.currentUser = "player1"    //tgl user
+        }
+
         switch(sender.tag){ // parse which button has been touched
         case 0:
             print("Button 0 Pressed")
-            Pos0.image = circle
+            Pos0.image = playerPiece
         case 1:
             print("Button 1 Pressed")
-            Pos1.image = circle
+            Pos1.image = playerPiece
         case 2:
             print("Button 2 Pressed")
-            Pos2.image = circle
+            Pos2.image = playerPiece
         case 3:
             print("Button 3 Pressed")
-            Pos3.image = circle
+            Pos3.image = playerPiece
         case 4:
             print("Button 4 Pressed")
-            Pos4.image = circle
+            Pos4.image = playerPiece
         case 5:
             print("Button 5 Pressed")
-            Pos5.image = circle
+            Pos5.image = playerPiece
         case 6:
             print("Button 6 Pressed")
-            Pos6.image = circle
+            Pos6.image = playerPiece
         case 7:
             print("Button 7 Pressed")
-            Pos7.image = circle
+            Pos7.image = playerPiece
         case 8:
             print("Button 8 Pressed")
-            Pos8.image = circle
+            Pos8.image = playerPiece
         default:
             print("Error in userPlacedPiece")
-
         }
-        
     }
-  
-    
 }
